071787a89692e47de259de00b297b023996e5abe
PowerSnail <hj@powersnail.com>
2023-03-10 16:45:08 -0800

    fix reload js

--- assets/js/reload.js
+++ assets/js/reload.js
@@ -1,22 +1,22 @@
-async function mainLoop() {
-  let lastModified = "";
-  
-  while (true) {
-    await new Promise(r => setTimeout(r, 2000));
-    try {
-      const response = await fetch("/_last_modified.json");
-      const data = await response.json();
-      const time = data["last_modified"];
-  
-      if (!lastModified) {
-        lastModified = time;
-      } else if (time !== lastModified) {
-        window.location.reload();
-      }
-    } catch (e) {
-      // Do nothing
+function mainLoop() {
+  let lastModified = undefined;
+
+  function processLastModified(newTime) {
+    if (lastModified === undefined) {
+      lastModified = newTime;
+    } else if (newTime !== lastModified) {
+      clearInterval(interval);
+      window.location.reload();
     }
   }
+
+  const interval = setInterval(
+    () =>
+      fetch("/_last_modified.txt")
+        .then((response) => response.text())
+        .then(processLastModified),
+    2000
+  );
 }
 
 mainLoop();
